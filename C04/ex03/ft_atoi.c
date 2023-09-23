c6fa37f6c75f13dbe9484920deac0876472c0687
akarichan00 <86488105+akarichan00@users.noreply.github.com>
2023-09-23 17:35:12 +0100

    ez

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,20 +6,48 @@
 /*   By: noben-ai <noben-ai@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 19:33:33 by noben-ai          #+#    #+#             */
-/*   Updated: 2023/09/21 19:39:58 by noben-ai         ###   ########.fr       */
+/*   Updated: 2023/09/23 15:44:19 by noben-ai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <stdio.h>
 int ft_atoi(char *str)
 {
     int i;
-    
+    int sign;
+    int result;
+
     i = 0;
-    while(str[i] != '\0')
+    sign = 1;
+    result = 0;
+    while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    while(str[i] == '-' || str[i] == '+')
     {
-        if(str[i] >= '0' && str[i] <= '9')
+        if(str[i] =='-')
+        {   
+            sign *= -1;
+            i++;
+        }
+        else if (str[i] == '+')
         {
-            str[i] 
+            i++;
         }
     }
+    while(str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+    {
+        result *= 10;
+        result += str[i] - '0';
+        i++;
+    }
+    return (result * sign);
+}
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        printf("result %i\n", ft_atoi(argv[1]));
+    }
+    
 }
